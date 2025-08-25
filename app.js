8d8ad375c0035de623d352ec1c582119be1962ea
Deepak Sharma <gaurp383@gmail.com>
2025-08-25 23:26:06 +0530

    Add files via upload
    tic tac toe game

--- app.js
+++ app.js
@@ -1,35 +1,91 @@
-let inputBox = document.getElementById('inputBox')
-let buttons = document.querySelectorAll('button')
+let boxes = document.querySelectorAll(".box");
+let resetBtn = document.querySelector("#reset-btn");
+let newGameBtn = document.querySelector("#new-btn");
+let msgContainer = document.querySelector(".msg-container");
+let msg = document.querySelector("#msg");
 
-let string = ''
+let turnO = true; //playerX, playerO
+let count = 0; //To Track Draw
 
-buttons.forEach(element =>{
-    element.addEventListener('click', (b)=>{
-        if(b.target.innerText == '='){
-         string = string == '' ? '0': string;
-         try{
-            string = String(eval(string))
-         inputBox.value = string;
-         }
-         catch(error){
-            inputBox.value = "Error(Press AC)"
-         }
-        }
-        else if(b.target.innerText == 'AC'){
-            string = ''
-            inputBox.value = string;
-        }
-        else if(b.target.innerText == 'DEL'){
-            string = string.substring(0,string.length-1)
-            inputBox.value = string;
-        }
-        else if(b.target.id == 'plusMinus'){
-            string = String(-eval(string))
-            inputBox.value = string;
-        }
-        else{
-            string += b.target.innerText
-            inputBox.value = string
-        }
-    })
-})
+const winPatterns = [
+  [0, 1, 2],
+  [0, 3, 6],
+  [0, 4, 8],
+  [1, 4, 7],
+  [2, 5, 8],
+  [2, 4, 6],
+  [3, 4, 5],
+  [6, 7, 8],
+];
+
+const resetGame = () => {
+  turnO = true;
+  count = 0;
+  enableBoxes();
+  msgContainer.classList.add("hide");
+};
+
+boxes.forEach((box) => {
+  box.addEventListener("click", () => {
+    if (turnO) {
+      //playerO
+      box.innerText = "O";
+      turnO = false;
+    } else {
+      //playerX
+      box.innerText = "X";
+      turnO = true;
+    }
+    box.disabled = true;
+    count++;
+
+    let isWinner = checkWinner();
+
+    if (count === 9 && !isWinner) {
+      gameDraw();
+    }
+  });
+});
+
+const gameDraw = () => {
+  msg.innerText = `Game was a Draw.`;
+  msgContainer.classList.remove("hide");
+  disableBoxes();
+};
+
+const disableBoxes = () => {
+  for (let box of boxes) {
+    box.disabled = true;
+  }
+};
+
+const enableBoxes = () => {
+  for (let box of boxes) {
+    box.disabled = false;
+    box.innerText = "";
+  }
+};
+
+const showWinner = (winner) => {
+  msg.innerText = `Congratulations, Winner is ${winner}`;
+  msgContainer.classList.remove("hide");
+  disableBoxes();
+};
+
+const checkWinner = () => {
+  for (let pattern of winPatterns) {
+    let pos1Val = boxes[pattern[0]].innerText;
+    let pos2Val = boxes[pattern[1]].innerText;
+    let pos3Val = boxes[pattern[2]].innerText;
+
+    if (pos1Val != "" && pos2Val != "" && pos3Val != "") {
+      if (pos1Val === pos2Val && pos2Val === pos3Val) {
+        showWinner(pos1Val);
+        return true;
+      }
+    }
+  }
+};
+
+newGameBtn.addEventListener("click", resetGame);
+resetBtn.addEventListener("click", resetGame);
